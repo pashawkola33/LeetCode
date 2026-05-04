b2390bd483658c31d41969713a5b7a98fb0e6ba3
Pavlo Sushkov <pashawkola33@gmail.com>
2026-05-04 22:51:43 +0300

    solved array_hashing problems

---
+++ src/arrays_hashing/ContainsDuplicate.java
@@ -0,0 +1,30 @@
+package arrays_hashing;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+
+
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> noDuplicates = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!noDuplicates.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        int[] array = {1, 1, 2, 3, 4, 5};
+
+
+        ContainsDuplicate solution = new ContainsDuplicate();
+
+
+
+        boolean hasDup = solution.containsDuplicate(array);
+        System.out.println(hasDup);
+    }
+}
---
+++ src/arrays_hashing/ValidAnagram.java
@@ -0,0 +1,32 @@
+package arrays_hashing;
+
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+       int count[] = new int[26];
+        if(s.length() != t.length()){
+            return false;
+        }
+        for(int i = 0; i < s.length(); i++){
+        count[s.charAt(i) - 'a']++;
+        }
+        for(int j = 0; j < t.length(); j++){
+            count[t.charAt(j) - 'a']--;
+        }
+        for(int x = 0; x < count.length; x++){
+            if(count[x] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidAnagram anagram = new ValidAnagram();
+
+
+        String as = "ass";
+        String sa = "sas";
+        boolean isanagram = anagram.isAnagram(as, sa);
+        System.out.println(isanagram);
+    }
+}
