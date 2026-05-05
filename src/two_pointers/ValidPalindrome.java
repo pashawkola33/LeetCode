a77f14a3cfeb5153994d93ab1b1a38ce172e6e3c
Pavlo Sushkov <pashawkola33@gmail.com>
2026-05-05 13:39:50 +0300

    125 Valid Palindrome Problem solved

--- src/two_pointers/ValidPalindrome.java
+++ src/two_pointers/ValidPalindrome.java
@@ -1,4 +1,33 @@
 package two_pointers;
 
+import java.util.Locale;
+
 public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
+
+            if (!Character.isLetterOrDigit(leftChar)) {
+                left++;
+            } else if (!Character.isLetterOrDigit(rightChar)) {
+                right--;
+            } else {
+                // Сюда мы попадем, только если оба символа — буквы/цифры
+                if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+
+    }
 }
