27f2944fb64fd5d199f573ca3782cab6d4130829
Pavlo Sushkov <pashawkola33@gmail.com>
2026-05-05 14:21:59 +0300

    20 Solved Valid Parenthases

--- src/stacks/ValidParentheses.java
+++ src/stacks/ValidParentheses.java
@@ -4,6 +4,33 @@ import java.util.Stack;
 
 public class ValidParentheses {
     public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ('[')) {
+                stack.push(c);
+            } else if (c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
 
+                if (c == ')' && top != '(') return false;
+                if (c == ']' && top != '[') return false;
+                if (c == '}' && top != '{') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+   public static void main(String[] args) {
+        ValidParentheses vp = new ValidParentheses();
+        String s = "{(())}";
+        boolean isvp = vp.isValid(s);
+        System.out.println(isvp);
     }
 }
+
