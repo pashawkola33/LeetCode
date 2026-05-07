837758e1a9de592d0d430765aab8f687d9252288
Pavlo Sushkov <pashawkola33@gmail.com>
2026-05-07 21:05:38 +0300

    104 Maximum Depth of Binary Tree

---
+++ src/binary_trees/MaximumDepthofBinaryTree.java
@@ -0,0 +1,13 @@
+package binary_trees;
+
+public class MaximumDepthofBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
