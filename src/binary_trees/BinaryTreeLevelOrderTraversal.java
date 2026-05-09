fee5616190869d5bf207859282b5d0b55c49b285
Pavlo Sushkov <pashawkola33@gmail.com>
2026-05-09 19:00:35 +0300

    102 Binary Tree Level Order Traversal Solved

---
+++ src/binary_trees/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,41 @@
+package binary_trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeLevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+
+            int levelSize = queue.size();
+
+            List<Integer> currentLevel = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode treeNode = queue.poll();
+                currentLevel.add(treeNode.val);
+
+                if (treeNode.left != null) {
+                    queue.add(treeNode.left);
+                }
+                if(treeNode.right != null){
+                    queue.add(treeNode.right);
+                }
+            }
+            result.add(currentLevel);
+        }
+        return result;
+    }
+}
